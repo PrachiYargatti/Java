45780ac0a4f6c8e5e68d6b3ab3bcb6157f3d9c84
Prachi Gajanan Yargatti <yaragattiprachi@gmail.com>
2025-06-24 11:44:58 +0530

    Create MaximumNumber.java

---
+++ MaximumNumber.java
@@ -0,0 +1,31 @@
+package day1;
+import java.util.Scanner;
+
+public class MaximumNumber {
+
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);	
+	    System.out.print("Enter three numbers: ");
+	    int a = sc.nextInt();
+	    int b = sc.nextInt();
+	    int c = sc.nextInt();
+	    
+	    if(a>=b) {
+	    	if(a>=c) {
+	    		System.out.println(a + " is maximum");
+	    	}
+	    	else {
+	    		System.out.println(c + " is maximum");
+	    	}
+	    }
+	    else{
+	    	if(b>=c) {
+	    		System.out.println(b + " is maximum");
+	    	}
+	    	else {
+	    		System.out.println(c + " is maximum");
+	    	}
+	    }
+	}
+
+}
